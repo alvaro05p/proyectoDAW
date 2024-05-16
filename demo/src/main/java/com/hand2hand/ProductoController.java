53726a91430a2ed8a4b476ca162106a06cfa6430
Alvaro Pardo <alvaro05pomg@gmail.com>
2024-05-16 12:31:39 +0200

    boton salir producto

---
+++ demo/src/main/java/com/hand2hand/ProductoController.java
@@ -0,0 +1,29 @@
+package com.hand2hand;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class ProductoController {
+    
+
+    @FXML
+    private Button botonSalir;
+
+    @FXML
+    private void salir() {
+        // Cambiar a la página principal
+        // Aquí abrimos la página Principal.fxml
+        try {
+            Stage stage = (Stage) botonSalir.getScene().getWindow();
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/com/hand2hand/fxml/Principal.fxml"))));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
